d2b60e03b3aa023125cb40fd617acba6668e99a3
Ababacar Sadikh GUEYE <49489598+Sadikh-SA@users.noreply.github.com>
2019-07-31 15:10:56 +0000

    Update Version20190725153128.php

--- src/Migrations/Version20190725153128.php
+++ src/Migrations/Version20190725153128.php
@@ -22,8 +22,8 @@ final class Version20190725153128 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE profil');
-        $this->addSql('ALTER TABLE utilisateur ADD profil VARCHAR(255) NOT NULL, DROP id_profil_id');
+        //$this->addSql('DROP TABLE profil');
+        //$this->addSql('ALTER TABLE utilisateur ADD profil VARCHAR(255) NOT NULL, DROP id_profil_id');
         $this->addSql('ALTER TABLE utilisateur ADD CONSTRAINT FK_1D1C63B36B2814EB FOREIGN KEY (id_parte_id) REFERENCES partenaire (id)');
         $this->addSql('CREATE INDEX IDX_1D1C63B36B2814EB ON utilisateur (id_parte_id)');
     }
@@ -33,9 +33,9 @@ final class Version20190725153128 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE profil (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
+        //$this->addSql('CREATE TABLE profil (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('ALTER TABLE utilisateur DROP FOREIGN KEY FK_1D1C63B36B2814EB');
         $this->addSql('DROP INDEX IDX_1D1C63B36B2814EB ON utilisateur');
-        $this->addSql('ALTER TABLE utilisateur ADD id_profil_id INT NOT NULL, DROP profil');
+        //$this->addSql('ALTER TABLE utilisateur ADD id_profil_id INT NOT NULL, DROP profil');
     }
 }
